d7a3d2699f2f79e5982923de65e0fbe229631c7e
Jacopo Costantini <jack@MacBook-Pro-di-Jacopo.local>
2026-01-23 21:55:58 +0100

    Updated Rails version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_29_165659) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_23_203107) do
   create_table "access_logs", force: :cascade do |t|
     t.integer "checkin_by_user_id", null: false
     t.datetime "created_at", null: false
